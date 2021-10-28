642c807229cf21fb513f634113beccb180120b6c
Aman Deep <deep.aman07.ad@gmail.com>
2021-10-29 03:59:41 +0530

    rows complete and frontend is also complete.

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -8,18 +8,32 @@ const HomeScreen = () => {
     <div>
       <Navbar />
       <Banner />
-      <Row
-        title='NETFLIX ORIGINALS'
-        fetchURL={requests.fetchNetflixOriginals}
-        islargeRow
-      />
-      <Row title='Trending Now' fetchURL={requests.fetchTrending} />
-      <Row title='Top Rated' fetchURL={requests.fetchTopRated} />
-      <Row title='Action Movies' fetchURL={requests.fetchActionMovies} />
-      <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
-      <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} />
-      <Row title='Romance Movies' fetchURL={requests.fetchRomanticMovies} />
-      <Row title='Documentaries' fetchURL={requests.fetchDocumentaries} />
+      <div style={{ marginTop: '-50px' }}>
+        <Row
+          title='NETFLIX ORIGINALS'
+          fetchURL={requests.fetchNetflixOriginals}
+          islargeRow
+        />
+        <Row
+          title='Trending Now'
+          fetchURL={requests.fetchTrending}
+          islargeRow
+        />
+        <Row title='Top Rated' fetchURL={requests.fetchTopRated} />
+        <Row
+          title='Action Movies'
+          fetchURL={requests.fetchActionMovies}
+          islargeRow
+        />
+        <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
+        <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} />
+        <Row
+          title='Romance Movies'
+          fetchURL={requests.fetchRomanticMovies}
+          islargeRow
+        />
+        <Row title='Documentaries' fetchURL={requests.fetchDocumentaries} />
+      </div>
     </div>
   )
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -2,7 +2,7 @@ import axios from './axios'
 import React, { useEffect, useState } from 'react'
 import '../style/Row.css'
 
-function Row({ title, fetchURL, isLargeRow = false }) {
+function Row({ title, fetchURL, islargeRow = false }) {
   const [movies, setMovies] = useState([])
   const baseURL = 'https://image.tmdb.org/t/p/original'
 
@@ -13,19 +13,24 @@ function Row({ title, fetchURL, isLargeRow = false }) {
     }
     fetchData()
   }, [fetchURL])
-  // console.log(movies)
   return (
     <div className='row text-white'>
-      <h2>{title}</h2>
-      <div className='row-poster'>
-        {movies.map((movie) => (
-          <img
-            src={`${baseURL}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+      <h2 className='topic'>{title}</h2>
+      <div className='row-posters'>
+        {movies.map(
+          (movie) =>
+            ((islargeRow && movie.poster_path) ||
+              (!islargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row-poster ${islargeRow && 'row-posterLarge'}`}
+                key={movie.id}
+                src={`${baseURL}${
+                  islargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
       </div>
     </div>
   )
